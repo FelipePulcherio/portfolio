1a98cf10661c77424e082e3760cf592333315dce
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2024-11-22 14:48:02 -0800

    Add extend to font-size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '3.5xl': ['2.0625rem', '2.375rem'],
+        '5xl': ['3rem', '6rem'],
+        '6xl': ['3.75rem', '6rem'],
+        '7xl': ['4.5rem', '6rem'],
+        '8xl': ['6rem', '6rem'],
+        '9xl': ['8rem', '6rem'],
+      },
+    },
   },
   plugins: [],
 };
