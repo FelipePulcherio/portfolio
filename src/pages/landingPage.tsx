1ed1993d471189db083c4f6aebdd0fb2a9d793b7
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2024-11-27 14:11:45 -0800

    Import all sections and add them to return statement

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -1,13 +1,21 @@
 import Header from '../components/header';
 import IntroSection from '../components/introSection';
 import KeywordsSection from '../components/keywordsSection';
+import SkillsSection from '../components/skillsSection';
+import FeaturedSection from '../components/featuredSection';
+import AboutSection from '../components/aboutSection';
+import Footer from '../components/footer';
 
 export default function LandingPage(): JSX.Element {
   return (
-    <div className='globalContainer p-[4px] sm:p-[6px] xl:p-[10px]'>
+    <div className='globalContainer px-[4px] pt-[4px] sm:px-[6px] sm:pt-[6px] xl:px-[10px] xl:pt-[10px]'>
       <Header />
       <IntroSection />
       <KeywordsSection />
+      <FeaturedSection />
+      <SkillsSection />
+      <AboutSection />
+      <Footer />
     </div>
   );
 }
