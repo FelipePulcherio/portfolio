a158d4e6d2db7b75c3393cec747a7b04e1bb297a
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2024-12-05 18:50:52 -0800

    Use hook for setting reference

--- src/components/contactSection.tsx
+++ src/components/contactSection.tsx
@@ -1,13 +1,17 @@
+import useSectionRefs from '../hooks/useSectionRefs';
 import SocialLinkedinSVG from '../assets/socialLinkedin.svg?react';
 import SocialGithubSVG from '../assets/socialGithub.svg?react';
 import SocialEmailSVG from '../assets/socialEmail.svg?react';
 import LogoSVG from '../assets/logo.svg?react';
 
 export default function ContactSection(): JSX.Element {
+  const { contact } = useSectionRefs();
+
   return (
     <div
       id='contactSection'
       className='aboutSection w-full h-min max-w-screen-2xl m-auto p-[30px] pb-8 rounded-t-[30px] font-sans bg-[#000000] text-[#FFFFFF] font-normal flex flex-col items-start justify-start gap-20 sm:px-[50px] sm:pt-[50px] md:px-[70px] lg:rounded-t-[45px] lg:px-[100px] xl:pt-[80px] 2xl:px-[150px]'
+      ref={contact}
     >
       <div className='mainContainer w-full h-min items-center justify-center flex flex-col gap-8 sm:gap-12 xl:gap-14'>
         <div className='logoContainer w-[125px] '>
