7573576126ed017f68311fd4548ae3d456168271
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2024-12-05 20:26:46 -0800

    Change styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,13 +2,13 @@ import ChevronUpSVG from '../assets/chevronUp.svg?react';
 
 export default function Footer(): JSX.Element {
   return (
-    <div id='footer' className='footer w-full h-min bg-transparent'>
-      <div className='footerContainer w-full h-full m-auto max-w-screen-2xl bg-[#000000] flex flex-col items-center justify-center '>
-        <hr className='w-[85%] h-[1px] mb-[30px] bg-[#999999] md:mb-8' />
+    <div id='footer' className='footer w-full h-full bg-transparent'>
+      <div className='footerContainer w-full h-full m-auto max-w-screen-2xl bg-[#000000] flex flex-col items-center justify-between '>
+        <hr className='w-[85%] h-[1px] bg-[#999999] ' />
         <div className='scrollUp w-[30px] aspect-square'>
           <ChevronUpSVG className='icon' width={'100%'} height={'100%'} />
         </div>
-        <div className='footerText text-sm text-[#FFFFFF] pb-2 pt-[30px] md:text-base xl:text-lg md:pb-4 md:pt-8'>
+        <div className='footerText text-sm text-[#FFFFFF] md:text-base xl:text-lg'>
           Developed by Felipe Pulcherio - 2024
         </div>
       </div>
