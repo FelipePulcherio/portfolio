863c2c9b10a0586baed95eac3904397e01c5dbee
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2024-12-05 18:37:57 -0800

    Use hook for setting reference

--- src/components/introSection.tsx
+++ src/components/introSection.tsx
@@ -1,8 +1,13 @@
+import useSectionRefs from '../hooks/useSectionRefs';
+
 export default function IntroSection(): JSX.Element {
+  const { intro } = useSectionRefs();
+
   return (
     <div
       id='introSection'
       className='w-full h-screen max-w-screen-2xl m-auto text-[#FFFFFF] text-xl leading-[2.25rem] font-light lg:text-2xl lg:leading-[2.5rem] xl:leading-[2.75rem] 2xl:text-3.5xl 2xl:leading-[3.25rem]'
+      ref={intro}
     >
       <div className='introSectionContainer h-full w-full max-w-screen-2xl mx-auto py-[70px] px-[30px] flex flex-col items-start justify-center gap-12 sm:px-[50px] md:py-[120px] md:px-[70px] lg:px-[100px] 2xl:px-[150px]'>
         <p className='font-sans paragraph'>Hey there!</p>
