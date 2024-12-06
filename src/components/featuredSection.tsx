6a98811d191e2506410a0edc3b60942b1f87becb
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2024-12-05 18:39:59 -0800

    Use hook for setting reference

--- src/components/featuredSection.tsx
+++ src/components/featuredSection.tsx
@@ -1,9 +1,12 @@
+import useSectionRefs from '../hooks/useSectionRefs';
 import CustomButton from './customButton';
 import WeddingImg from '../assets/wedding.png';
 import GameImg from '../assets/game.png';
 import WebscraperImg from '../assets/pricewebscraper.png';
 
 export default function FeaturedSection(): JSX.Element {
+  const { featured } = useSectionRefs();
+
   function OpenLink(url: string): void {
     window.open(url, '_blank');
   }
@@ -68,6 +71,7 @@ export default function FeaturedSection(): JSX.Element {
     <div
       id='featuredSection'
       className='featuredSection w-full h-min max-w-screen-2xl m-auto p-[30px] pb-[100px] rounded-[30px] font-sans bg-[#FFFFFF] text-[#101010] font-normal flex flex-col items-start justify-start gap-12 sm:px-[50px] md:px-[70px] md:pt-[40px] lg:rounded-[45px] lg:px-[100px] xl:pt-[80px] 2xl:px-[150px]'
+      ref={featured}
     >
       <div className='title font-bold w-full text-3xl text-center md:text-4xl md:text-left 2xl:text-5xl '>
         Featured Projects
