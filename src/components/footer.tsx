97c59ab7a6314851e8a881014f88509ba7a2d23a
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2024-12-05 20:28:17 -0800

    Add onClick in svg image

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,21 @@
+import useSectionRefs from '../hooks/useSectionRefs';
 import ChevronUpSVG from '../assets/chevronUp.svg?react';
 
 export default function Footer(): JSX.Element {
+  const { intro } = useSectionRefs();
   return (
     <div id='footer' className='footer w-full h-full bg-transparent'>
       <div className='footerContainer w-full h-full m-auto max-w-screen-2xl bg-[#000000] flex flex-col items-center justify-between '>
         <hr className='w-[85%] h-[1px] bg-[#999999] ' />
         <div className='scrollUp w-[30px] aspect-square'>
-          <ChevronUpSVG className='icon' width={'100%'} height={'100%'} />
+          <ChevronUpSVG
+            className='icon'
+            width={'100%'}
+            height={'100%'}
+            onClick={() =>
+              intro.current?.scrollIntoView({ behavior: 'smooth' })
+            }
+          />
         </div>
         <div className='footerText text-sm text-[#FFFFFF] md:text-base xl:text-lg'>
           Developed by Felipe Pulcherio - 2024
